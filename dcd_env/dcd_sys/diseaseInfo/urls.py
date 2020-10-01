4c38af32a683b4ae6dd48fd643b47d26a5f3e647
DanielGraham123 <dg.danielboaz@yahoo.com>
2020-10-01 11:54:53 +0100

    added disease search functionality

--- dcd_env/dcd_sys/diseaseInfo/urls.py
+++ dcd_env/dcd_sys/diseaseInfo/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home
+from .views import home, search
 
 app_name = "diseaseInfo"
 
 urlpatterns = [
-    path('', home, name="home-view")
+    path('', home, name="home"),
+    path('search_results', search, name="search")
 ]
--- dcd_env/dcd_sys/diseaseInfo/views.py
+++ dcd_env/dcd_sys/diseaseInfo/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.conf import settings
-import os
-import json
 from .models import Disease, Drug
+
+from django.db.models import Q
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 
 def home(request):
     diseases_fil = Disease.objects.all().order_by('id')[:12]
@@ -18,4 +20,30 @@ def home(request):
         'drugs': drugs,
     }
     return render(request, 'diseaseInfo/main.html', context)
+
+def search(request):
+    query = request.GET['searchQuery']
+
+    if query:
+        result_set = Disease.objects.filter(Q(name__icontains=query) | Q(description__icontains=query))
+    else:
+        result_set = Disease.objects.filter()
+
+    page = request.GET.get('page', 1)
+
+    paginator = Paginator(result_set, 10)
+
+    try:
+        results = paginator.page(page)
+    except PageNotAnInteger:
+        results = paginator.page(1)
+    except EmptyPage:
+        results = paginator.page(paginator.num_pages)
+
+    context = {
+        'results': results,
+        'query': query,
+    }
+
+    return render(request, 'diseaseInfo/search_results.html', context)
 
